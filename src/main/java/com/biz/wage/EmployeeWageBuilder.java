ef1e717cfc3de63650987c3ccce1b4b484a32660
Rushiraj11 <grushiraj95@gmail.com>
2021-09-03 19:39:15 +0530

    Added:Employee Attendence check

--- src/main/java/com/biz/wage/EmployeeWageBuilder.java
+++ src/main/java/com/biz/wage/EmployeeWageBuilder.java
@@ -1,9 +1,21 @@
 package com.biz.wage;
 
 public class EmployeeWageBuilder {
+    static final int IS_FULL_TIME = 1;
+
 
     public static void main(String[] args) {
 
         System.out.println("Welcome to Employee Wage Computation");
+        empAttendence();
     }
-}
+
+      public static void empAttendence(){
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+            if (empCheck == IS_FULL_TIME)
+                System.out.println("Employee is present");
+            else
+                System.out.println("Employee is Absent");
+        }
+    }
+
