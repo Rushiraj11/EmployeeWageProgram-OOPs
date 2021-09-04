86899c19c0f21da761bc7fb022d42f9b72ca71b8
Rushiraj11 <grushiraj95@gmail.com>
2021-09-04 19:52:57 +0530

    Added Part time Employee Wage

--- src/main/java/com/biz/wage/EmployeeWageBuilder.java
+++ src/main/java/com/biz/wage/EmployeeWageBuilder.java
@@ -2,6 +2,7 @@ package com.biz.wage;
 
 public class EmployeeWageBuilder {
     static final int IS_FULL_TIME = 1;
+    static final int IS_PART_TIME = 2;
     static final int EMP_RATE_PER_HOUR = 20;
    static int empHrs = 0;
    static int empWage = 0;
@@ -16,10 +17,13 @@ public class EmployeeWageBuilder {
     }
 
       public static void empAttendence() {
-          double empCheck = Math.floor(Math.random() * 10) % 2;
+          double empCheck = Math.floor(Math.random() * 10) % 3;
           if (empCheck == IS_FULL_TIME) {
-              System.out.println("Employee is present");
+              System.out.println("Employee is Working FullTime");
               empHrs = 8;
+          } else if (empCheck== IS_PART_TIME) {
+              empHrs = 4;
+              System.out.println("Employee is Working PartTime");
           } else {
               System.out.println("Employee is Absent");
               empHrs = 0;
@@ -32,12 +36,4 @@ public class EmployeeWageBuilder {
           }
 
 
-
-
 }
-
-
-
-
-
-
