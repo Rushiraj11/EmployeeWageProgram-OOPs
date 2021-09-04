95e495eb60becc5101762e23a66571441325df83
Rushiraj11 <grushiraj95@gmail.com>
2021-09-04 20:14:17 +0530

    Added:Switch case to get EmployeeDailyWage

--- src/main/java/com/biz/wage/EmployeeWageBuilder.java
+++ src/main/java/com/biz/wage/EmployeeWageBuilder.java
@@ -4,10 +4,8 @@ public class EmployeeWageBuilder {
     static final int IS_FULL_TIME = 1;
     static final int IS_PART_TIME = 2;
     static final int EMP_RATE_PER_HOUR = 20;
-   static int empHrs = 0;
-   static int empWage = 0;
-
-
+    static int empHrs = 0;
+    static int empWage = 0;
 
     public static void main(String[] args) {
 
@@ -16,24 +14,28 @@ public class EmployeeWageBuilder {
         empDailyWage();
     }
 
-      public static void empAttendence() {
-          double empCheck = Math.floor(Math.random() * 10) % 3;
-          if (empCheck == IS_FULL_TIME) {
-              System.out.println("Employee is Working FullTime");
-              empHrs = 8;
-          } else if (empCheck== IS_PART_TIME) {
-              empHrs = 4;
-              System.out.println("Employee is Working PartTime");
-          } else {
-              System.out.println("Employee is Absent");
-              empHrs = 0;
-          }
-      }
+    public static void empAttendence() {
+        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+        switch (empCheck) {
+            case IS_FULL_TIME:
+                System.out.println("Employee working FullTime");
+                empHrs = 8;
+                break;
+            case IS_PART_TIME:
+                System.out.println("Employee working PartTime");
+                empHrs = 4;
+                break;
+            default:
+                System.out.println("Employee is Absent");
+                empHrs = 0;
 
-          public static void empDailyWage() {
-              empWage = empHrs * EMP_RATE_PER_HOUR;
-              System.out.println("Emp Wage: " + empWage);
-          }
+        }
+    }
+
+        public static void empDailyWage() {
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            System.out.println("Emp Wage: " + empWage);
+        }
 
 
-}
+    }
