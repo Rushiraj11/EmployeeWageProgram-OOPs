77f94151fe35c06eb5a9080b06ebfd08c0727fd0
Rushiraj11 <grushiraj95@gmail.com>
2021-09-05 18:36:44 +0530

    Added:Daily wage for 20 days

--- src/main/java/com/biz/wage/EmployeeWageBuilder.java
+++ src/main/java/com/biz/wage/EmployeeWageBuilder.java
@@ -4,38 +4,36 @@ public class EmployeeWageBuilder {
     static final int IS_FULL_TIME = 1;
     static final int IS_PART_TIME = 2;
     static final int EMP_RATE_PER_HOUR = 20;
+    public static final int NUM_OF_WORKING_DAYS = 20;
     static int empHrs = 0;
     static int empWage = 0;
+    static int totalEmpWage = 0;
 
     public static void main(String[] args) {
 
-        System.out.println("Welcome to Employee Wage Computation");
-        empAttendence();
-        empDailyWage();
-    }
+        int empHrs = 0;
+        int empWage = 0;
+        int totalEmpWage = 0;
+        //computation
+        for (int day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+            int empCheck = (int) Math.floor(Math.random() * 10) % 4;
+            switch (empCheck) {
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                default:
+                    empHrs = 0;
 
-    public static void empAttendence() {
-        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-        switch (empCheck) {
-            case IS_FULL_TIME:
-                System.out.println("Employee working FullTime");
-                empHrs = 8;
-                break;
-            case IS_PART_TIME:
-                System.out.println("Employee working PartTime");
-                empHrs = 4;
-                break;
-            default:
-                System.out.println("Employee is Absent");
-                empHrs = 0;
+            }
 
-        }
-    }
-
-        public static void empDailyWage() {
             empWage = empHrs * EMP_RATE_PER_HOUR;
+            totalEmpWage = totalEmpWage + empWage;
             System.out.println("Emp Wage: " + empWage);
         }
-
-
+        System.out.println("Total Emp Wage for 20 days:" + totalEmpWage);
     }
+}
+
